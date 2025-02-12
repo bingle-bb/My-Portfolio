55ac520da884536c5c51554329447717ee579a22
revy <revy200311@gmail.com>
2025-02-12 13:54:19 +0630

    about update

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".progress-circle").forEach((circle) => {
-    let progress = circle.getAttribute("data-progress");
-    let span = document.createElement("span");
-    circle.appendChild(span);
-    let rotation = (progress / 100) * 360;
-    span.style.transform = `rotate(${rotation}deg)`;
-  });
-});
